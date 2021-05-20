a56fac071e6c1399e6bff8aaf81d8544d8c3f7ea
Gregory Becker <igreg@users.noreply.github.com>
2021-05-20 17:22:16 +0100

    Allow options block to be defined in stages (#63)
    As reported by issue #55, the `options` block can be defined more than once.
    
    Co-authored-by: Szymon Gwóźdź <5880551+sgwozdz@users.noreply.github.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -258,7 +258,7 @@ function validateTextDocument(textDocument: TextDocument) {
   
 	getAllowOnceDiagnostics(allowOncePipelinesBlock, textWithoutComments, textDocument, diagnostics);
 	// getAllowOnceDiagnostics(allowOnceStagesBlock, textWithoutComments, textDocument, diagnostics);
-	getAllowOnceDiagnostics(allowOnceOptionsBlock, textWithoutComments, textDocument, diagnostics);
+	// getAllowOnceDiagnostics(allowOnceOptionsBlock, textWithoutComments, textDocument, diagnostics);
 	getAllowOnceDiagnostics(allowOnceParametersBlock, textWithoutComments, textDocument, diagnostics);
 	getAllowOnceDiagnostics(allowOnceTriggersBlock, textWithoutComments, textDocument, diagnostics);
 
